0cb43bccd5c68d66b0f8079afbe3f3385c937c46
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-05-26 12:07:36 -0600

    Teclado.h

---
+++ Teclado.h
@@ -0,0 +1,14 @@
+#ifndef TECLADO_H
+#define TECLADO_H
+
+class Teclado : public Periferico {
+public:
+    Teclado(string _nombre, int _precio) : Periferico(_nombre, _precio) {}
+
+    void mostrarInformacion() {
+        cout << "Teclado: " << nombre << endl;
+        cout << "Precio: $" << precio << endl;
+    }
+};
+
+#endif
