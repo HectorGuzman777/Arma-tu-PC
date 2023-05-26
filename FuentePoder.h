8d8ed87b68d52bbe339ff953292d340cf9f9ebbf
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-05-26 12:04:14 -0600

    FuentePoder.h

---
+++ FuentePoder.h
@@ -0,0 +1,14 @@
+#ifndef FUENTEPODER_H
+#define FUENTEPODER_H
+
+class FuentePoder : public Producto {
+public:
+    FuentePoder(string _nombre, int _precio) : Producto(_nombre, _precio) {}
+
+    void mostrarInformacion() {
+        cout << "Fuente de poder: " << nombre << endl;
+        cout << "Precio: $" << precio << endl;
+    }
+};
+
+#endif
