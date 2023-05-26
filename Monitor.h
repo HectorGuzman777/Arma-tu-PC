b7b6e3c9a93205701a3f9e61a0a69b3431296db6
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-05-26 12:06:58 -0600

    Monitor.h

---
+++ Monitor.h
@@ -0,0 +1,13 @@
+#ifndef MONITOR_H
+#define MONITOR_H
+
+class Monitor : public Periferico {
+public:
+    Monitor(string _nombre, int _precio) : Periferico(_nombre, _precio) {}
+
+    void mostrarInformacion() {
+        cout << "Monitor: " << nombre << endl;
+        cout << "Precio: $" << precio << endl;
+    }
+};
+#endif
