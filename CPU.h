4ffd5a6130802a7711ec5c9f6b0fc7b4c4ab502d
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-05-26 12:01:19 -0600

    CPU.h

---
+++ CPU.h
@@ -0,0 +1,14 @@
+#ifndef CPU_H
+#define CPU_H
+
+class CPU : public Producto {
+public:
+    CPU(string _nombre, int _precio) : Producto(_nombre, _precio) {}
+
+    void mostrarInformacion() {
+        cout << "CPU: " << nombre << endl;
+        cout << "Precio: $" << precio << endl;
+    }
+};
+
+#endif
