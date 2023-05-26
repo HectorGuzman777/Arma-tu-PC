4ff07b9624a236b7b516678db74af6579b7a3e5c
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-05-26 12:05:16 -0600

    RAM.h

---
+++ RAM.h
@@ -0,0 +1,14 @@
+#ifndef RAM_H
+#define RAM_H
+
+class RAM : public Producto {
+public:
+    RAM(string _nombre, int _precio) : Producto(_nombre, _precio) {}
+
+    void mostrarInformacion() {
+        cout << "RAM: " << nombre << endl;
+        cout << "Precio: $" << precio << endl;
+    }
+};
+
+#endif
