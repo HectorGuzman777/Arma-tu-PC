2f0fefe756e9f42c77d5efab6c356e26a7e26516
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-05-26 12:02:58 -0600

    Motherboard.h

---
+++ Motherboard.h
@@ -0,0 +1,14 @@
+#ifndef MOTHERBOARD_H
+#define MOTHERBOARD_H
+
+class Motherboard : public Producto {
+public:
+    Motherboard(string _nombre, int _precio) : Producto(_nombre, _precio) {}
+
+    void mostrarInformacion() {
+        cout << "Motherboard: " << nombre << endl;
+        cout << "Precio: $" << precio << endl;
+    }
+};
+
+#endif
