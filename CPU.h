19af73c845480b274fb892d610a4bf30e30adcfb
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-06-01 14:32:50 -0600

    Update CPU.h

--- CPU.h
+++ CPU.h
@@ -2,12 +2,11 @@
 #define CPU_H
 #include <iostream>
 
-
 class CPU : public Producto {
 public:
-    CPU(string _nombre, int _precio) : Producto(_nombre, _precio) {}
+    CPU(string _nombre, int _precio) : Producto(_nombre, _precio) {} //Constructor de CPU y herencia de la clase Producto para su nombre y precio
 
-    void mostrarInformacion() {
+    void mostrarInformacion() { //Muestra la información del producto 
         cout << "CPU: " << nombre << endl;
         cout << "Precio: $" << precio << endl;
     }
