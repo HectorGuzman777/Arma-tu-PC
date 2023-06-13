7b2d33c475cd3882642b10780723611ebd3ba0b9
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-06-13 10:46:00 -0600

    Update Monitor.h

--- Monitor.h
+++ Monitor.h
@@ -1,12 +1,23 @@
+/*
+ *
+ * Proyecto Arma tu PC
+ * Héctor Gúmaro Guzmán Reyes
+ * A01710706
+ * 13/06/2023
+ * version: final
+ * Clase hija de la clase Periferico
+ */
+
 #ifndef MONITOR_H
 #define MONITOR_H
 #include <iostream>
-
+//declaración de la clase hija
 class Monitor : public Periferico {
+    //metodos de la clase
 public:
-    Monitor(string _nombre, int _precio) : Periferico(_nombre, _precio) {}
+    Monitor(string _nombre, int _precio) : Periferico(_nombre, _precio) {} //constructor
 
-    void mostrarInformacion() {
+    void mostrarInformacion() { //Metodo que despliega la frecuencia del monitor y su precio
         cout << "Monitor: " << nombre << endl;
         cout << "Precio: $" << precio << endl;
     }
