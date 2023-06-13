a1e9167e977e03ee27997087cba6cffe0ed5870d
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-06-13 10:08:24 -0600

    Update FuentePoder.h

--- FuentePoder.h
+++ FuentePoder.h
@@ -1,12 +1,23 @@
+/*
+ *
+ * Proyecto Arma tu PC
+ * Héctor Gúmaro Guzmán Reyes
+ * A01710706
+ * 13/06/2023
+ * version: final
+ * Clase hija de la clase Producto
+ */
+
 #ifndef FUENTEPODER_H
 #define FUENTEPODER_H
 #include <iostream>
-
+//declaración de la clase hija de la clase Producto
 class FuentePoder : public Producto {
+    //Metodos de la clase
 public:
-    FuentePoder(string _nombre, int _precio) : Producto(_nombre, _precio) {}
+    FuentePoder(string _nombre, int _precio) : Producto(_nombre, _precio) {} //COnstructor
 
-    void mostrarInformacion() {
+    void mostrarInformacion() { //metodo que desplegará la capacidad de la fuente de poder y su precio
         cout << "Fuente de poder: " << nombre << endl;
         cout << "Precio: $" << precio << endl;
     }
