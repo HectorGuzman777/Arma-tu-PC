274ff1c605475d396e6982f5356e31a607aa9668
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-06-13 10:13:39 -0600

    Update GPU.h

--- GPU.h
+++ GPU.h
@@ -1,7 +1,17 @@
+/*
+ *
+ * Proyecto Arma tu PC
+ * Héctor Gúmaro Guzmán Reyes
+ * A01710706
+ * 13/06/2023
+ * version: final
+ * Clase hija de la clase Producto
+ */
+
 #ifndef GPU_H
 #define GPU_H
 #include <iostream>
-
+//
 class GPU : public Producto {
 public:
     GPU(string _nombre, int _precio) : Producto(_nombre, _precio) {}
