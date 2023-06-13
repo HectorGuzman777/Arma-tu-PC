867c7077faff5bd68363984ae74a1624e83c4c31
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-06-13 10:37:28 -0600

    Update Gabinete.h

--- Gabinete.h
+++ Gabinete.h
@@ -1,12 +1,23 @@
+/*
+ *
+ * Proyecto Arma tu PC
+ * Héctor Gúmaro Guzmán Reyes
+ * A01710706
+ * 13/06/2023
+ * version: final
+ * Clase hija de la clase Producto
+ */
+
 #ifndef GABINETE_H
 #define GABINETE_H
 #include <iostream>
-
+//declaración de la clase hija
 class Gabinete : public Producto {
+    //metodos
 public:
-    Gabinete(string _nombre, int _precio) : Producto(_nombre, _precio) {}
+    Gabinete(string _nombre, int _precio) : Producto(_nombre, _precio) {} //constructor
 
-    void mostrarInformacion() {
+    void mostrarInformacion() { //metodo que despliega el tipo de gabinete y su precio
         cout << "Gabinete: " << nombre << endl;
         cout << "Precio: $" << precio << endl;
     }
