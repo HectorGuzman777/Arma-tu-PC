b4910ed2efc00bff5a51ae7a56a43056e0fcd3ff
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-06-13 10:01:47 -0600

    Update Disipador.h

--- Disipador.h
+++ Disipador.h
@@ -1,3 +1,13 @@
+/*
+ *
+ * Proyecto Arma tu PC
+ * Héctor Gúmaro Guzmán Reyes
+ * A01710706
+ * 13/06/2023
+ * version: final
+ * Clase hija de la clase Producto
+ */
+
 #ifndef DISIPADOR_H
 #define DISIPADOR_H
 #include <iostream>
