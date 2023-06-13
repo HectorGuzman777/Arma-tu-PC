3d5a39efd3b1557aa837ee56e5e43743c0e6cab8
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-06-13 10:49:10 -0600

    Update Motherboard.h

--- Motherboard.h
+++ Motherboard.h
@@ -1,13 +1,24 @@
+/*
+ *
+ * Proyecto Arma tu PC
+ * Héctor Gúmaro Guzmán Reyes
+ * A01710706
+ * 13/06/2023
+ * version: final
+ * Clase hija de la clase Producto
+ */
+
 #ifndef MOTHERBOARD_H
 #define MOTHERBOARD_H
 #include <iostream>
 
-
+//declaración de la clase hija
 class Motherboard : public Producto {
+    //metodos
 public:
-    Motherboard(string _nombre, int _precio) : Producto(_nombre, _precio) {}
+    Motherboard(string _nombre, int _precio) : Producto(_nombre, _precio) {} //constructor
 
-    void mostrarInformacion() {
+    void mostrarInformacion() { //metodo que despliega el tipo de motherboard y su precio
         cout << "Motherboard: " << nombre << endl;
         cout << "Precio: $" << precio << endl;
     }
