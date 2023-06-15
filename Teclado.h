184b3831c8389058199fc7652f6bb1f1ed1c73e3
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-06-15 10:36:55 -0600

    Update Teclado.h

--- Teclado.h
+++ Teclado.h
@@ -1,12 +1,22 @@
+/*
+ *
+ * Proyecto Arma tu PC
+ * Héctor Gúmaro Guzmán Reyes
+ * A01710706
+ * 13/06/2023
+ * version: final
+ * Clase hija de la clase Periferico
+ */
+
 #ifndef TECLADO_H
 #define TECLADO_H
 #include <iostream>
-
+//declaración de la clasae hija
 class Teclado : public Periferico {
 public:
-    Teclado(string _nombre, int _precio) : Periferico(_nombre, _precio) {}
+    Teclado(string _nombre, int _precio) : Periferico(_nombre, _precio) {} //constructor
 
-    void mostrarInformacion() {
+    void mostrarInformacion() { //metodo para mostrar el tipo y precio
         cout << "Teclado: " << nombre << endl;
         cout << "Precio: $" << precio << endl;
     }
