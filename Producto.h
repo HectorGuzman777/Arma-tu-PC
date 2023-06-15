7c74f493c5812cf84e8907ecdd10cbb90eb92c48
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-06-15 10:29:58 -0600

    Update Producto.h

--- Producto.h
+++ Producto.h
@@ -1,20 +1,31 @@
+/*
+ *
+ * Proyecto Arma tu PC
+ * Héctor Gúmaro Guzmán Reyes
+ * A01710706
+ * 13/06/2023
+ * version: final
+ * Clase padre para las clases Periferico, CPU, Disipador, FuentePoder, GPU, Gabinete, Motherboard y RAM
+ *Clase abstracta
+ */
+
 #ifndef PRODUCTO_H
 #define PRODUCTO_H
 #include <iostream>
 
 using namespace std;
-
+//Declaración de la clase
 class Producto {
 protected:
     string nombre;
     int precio;
 
 public:
-    Producto(string _nombre, int _precio) : nombre(_nombre), precio(_precio) {}
-
+    Producto(string _nombre, int _precio) : nombre(_nombre), precio(_precio) {} //constructor
+//abstracción
     virtual void mostrarInformacion() = 0;  // Método virtual puro
 
-    int getPrecio() {
+    int getPrecio() { //Obtiene el precio de los productos
         return precio;
     }
 };
