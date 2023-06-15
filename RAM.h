4484557af2b6f69ae8d8a53c24a75ef6f82d84f7
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-06-15 10:33:18 -0600

    Update RAM.h

--- RAM.h
+++ RAM.h
@@ -1,12 +1,22 @@
+/*
+ *
+ * Proyecto Arma tu PC
+ * Héctor Gúmaro Guzmán Reyes
+ * A01710706
+ * 13/06/2023
+ * version: final
+ * Clase hija de la clase Producto
+ */
+
 #ifndef RAM_H
 #define RAM_H
 #include <iostream>
-
+//declaración de la clase hija
 class RAM : public Producto {
 public:
-    RAM(string _nombre, int _precio) : Producto(_nombre, _precio) {}
+    RAM(string _nombre, int _precio) : Producto(_nombre, _precio) {} //constructor
 
-    void mostrarInformacion() {
+    void mostrarInformacion() { //metodo que despliega la capacidad y su precio
         cout << "RAM: " << nombre << endl;
         cout << "Precio: $" << precio << endl;
     }
